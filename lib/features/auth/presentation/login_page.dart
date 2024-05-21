d20676665282f4b385d7e92287be2602bd9fa96a
Khanin Chomphusri <khanin-personal@Khanins-MacBook-Air.local>
2024-05-21 13:22:59 +0700

    remove app bar and remove padding

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -17,70 +17,73 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Row(
-        children: [
-          const Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(32.0),
+      // appBar: AppBar(
+      //   title: const Text('Login'),
+      // ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 40, bottom: 40),
+        child: Row(
+          children: [
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'FinMaker',
+                      style: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const VerticalDivider(),
+            Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'FinMaker',
-                    style: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
+                  const Text('Login'),
+                  Form(
+                    child: SizedBox(
+                      width: 360,
+                      child: Column(
+                        children: [
+                          TextFormField(),
+                          TextFormField(),
+                          const SizedBox(height: 20),
+                          ElevatedButton(onPressed: () {}, child: Text('Login'))
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    indent: 40,
+                    endIndent: 40,
+                    height: 80,
+                  ),
+                  ElevatedButton(
+                    style: kLoginButtonStyle,
+                    onPressed: () {},
+                    child: const Text('Apple'),
+                  ),
+                  const SizedBox(height: 12),
+                  ElevatedButton(
+                    style: kLoginButtonStyle,
+                    onPressed: () {},
+                    child: const Text('Facebook'),
+                  ),
+                  const SizedBox(height: 12),
+                  ElevatedButton(
+                    style: kLoginButtonStyle,
+                    onPressed: () {},
+                    child: const Text('Google'),
                   ),
                 ],
               ),
             ),
-          ),
-          const VerticalDivider(),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Login'),
-                Form(
-                  child: SizedBox(
-                    width: 360,
-                    child: Column(
-                      children: [
-                        TextFormField(),
-                        TextFormField(),
-                        const SizedBox(height: 20),
-                        ElevatedButton(onPressed: () {}, child: Text('Login'))
-                      ],
-                    ),
-                  ),
-                ),
-                const Divider(
-                  indent: 40,
-                  endIndent: 40,
-                  height: 80,
-                ),
-                ElevatedButton(
-                  style: kLoginButtonStyle,
-                  onPressed: () {},
-                  child: const Text('Apple'),
-                ),
-                const SizedBox(height: 12),
-                ElevatedButton(
-                  style: kLoginButtonStyle,
-                  onPressed: () {},
-                  child: const Text('Facebook'),
-                ),
-                const SizedBox(height: 12),
-                ElevatedButton(
-                  style: kLoginButtonStyle,
-                  onPressed: () {},
-                  child: const Text('Google'),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

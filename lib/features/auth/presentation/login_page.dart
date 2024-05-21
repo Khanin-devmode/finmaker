27bced35c865b0db5dd23c9f93c1b3543c885788
Khanin Chomphusri <khanin-personal@Khanins-MacBook-Air.local>
2024-05-21 15:11:23 +0700

    login page done

--- lib/features/auth/presentation/login_page.dart
+++ lib/features/auth/presentation/login_page.dart
@@ -27,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
         padding: const EdgeInsets.only(top: 40, bottom: 40),
         child: Row(
           children: [
-            Expanded(
+            const Expanded(
               child: Padding(
                 padding: EdgeInsets.all(32.0),
                 child: Column(
@@ -37,76 +37,6 @@ class _LoginPageState extends State<LoginPage> {
                       'FinMaker',
                       style:
                           TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
-                    ),
-                    BlocConsumer<AuthCubit, AuthState>(
-                      builder: (context, state) {
-                        if (state is AuthInitial || state is AuthLoading) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (state is AuthAuthenticated) {
-                          return Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Welcome, ${state.email}'),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    context.read<AuthCubit>().signOut();
-                                  },
-                                  child: const Text('Sign Out'),
-                                ),
-                              ],
-                            ),
-                          );
-                        } else if (state is AuthUnauthenticated) {
-                          return Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                TextField(
-                                  controller: _emailController,
-                                  decoration:
-                                      const InputDecoration(labelText: 'Email'),
-                                ),
-                                TextField(
-                                    controller: _passwordController,
-                                    decoration: const InputDecoration(
-                                        labelText: 'Password'),
-                                    obscureText: true),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    final email = _emailController.text;
-                                    final password = _passwordController.text;
-                                    context
-                                        .read<AuthCubit>()
-                                        .signIn(email, password);
-                                  },
-                                  child: const Text('Sign In'),
-                                ),
-                              ],
-                            ),
-                          );
-                        } else {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        }
-                      },
-                      listener: (context, state) {
-                        if (state is AuthAuthenticated) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Welcome ${state.email}'),
-                            ),
-                          );
-                        } else if (state is AuthError) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Error: ${state.message}'),
-                            ),
-                          );
-                        }
-                      },
                     ),
                   ],
                 ),
@@ -118,43 +48,109 @@ class _LoginPageState extends State<LoginPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const Text('Login'),
-                  Form(
-                    child: SizedBox(
-                      width: 360,
-                      child: Column(
-                        children: [
-                          TextFormField(),
-                          TextFormField(),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Login'),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  const Divider(
-                    indent: 40,
-                    endIndent: 40,
-                    height: 80,
-                  ),
-                  ElevatedButton(
-                    style: kLoginButtonStyle,
-                    onPressed: () {},
-                    child: const Text('Apple'),
-                  ),
-                  const SizedBox(height: 12),
-                  ElevatedButton(
-                    style: kLoginButtonStyle,
-                    onPressed: () {},
-                    child: const Text('Facebook'),
-                  ),
-                  const SizedBox(height: 12),
-                  ElevatedButton(
-                    style: kLoginButtonStyle,
-                    onPressed: () {},
-                    child: const Text('Google'),
+                  BlocConsumer<AuthCubit, AuthState>(
+                    builder: (context, state) {
+                      if (state is AuthInitial || state is AuthLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (state is AuthAuthenticated) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('Welcome, ${state.email}'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.read<AuthCubit>().signOut();
+                                },
+                                child: const Text('Sign Out'),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else if (state is AuthUnauthenticated) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Form(
+                                child: SizedBox(
+                                  width: 320,
+                                  child: Column(
+                                    children: [
+                                      TextField(
+                                        controller: _emailController,
+                                        decoration: const InputDecoration(
+                                          labelText: 'Email',
+                                        ),
+                                      ),
+                                      TextField(
+                                        controller: _passwordController,
+                                        decoration: const InputDecoration(
+                                            labelText: 'Password'),
+                                        obscureText: true,
+                                      ),
+                                      const SizedBox(height: 20),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          final email = _emailController.text;
+                                          final password =
+                                              _passwordController.text;
+                                          context
+                                              .read<AuthCubit>()
+                                              .signIn(email, password);
+                                        },
+                                        child: const Text('Sign In'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const Divider(
+                                indent: 40,
+                                endIndent: 40,
+                                height: 80,
+                              ),
+                              ElevatedButton(
+                                style: kLoginButtonStyle,
+                                onPressed: () {},
+                                child: const Text('Apple'),
+                              ),
+                              const SizedBox(height: 12),
+                              ElevatedButton(
+                                style: kLoginButtonStyle,
+                                onPressed: () {},
+                                child: const Text('Facebook'),
+                              ),
+                              const SizedBox(height: 12),
+                              ElevatedButton(
+                                style: kLoginButtonStyle,
+                                onPressed: () {},
+                                child: const Text('Google'),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    },
+                    listener: (context, state) {
+                      if (state is AuthAuthenticated) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Welcome ${state.email}'),
+                          ),
+                        );
+                      } else if (state is AuthError) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Error: ${state.message}'),
+                          ),
+                        );
+                      }
+                    },
                   ),
                 ],
               ),
